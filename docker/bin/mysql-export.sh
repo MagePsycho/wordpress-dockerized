59f389be916b6f92744d17ce47b447a5a22850f8
Raj KB <rajendra.bhatta@citruss.com>
2019-01-14 14:16:15 +0400

    Fix export path

--- docker/bin/mysql-export.sh
+++ docker/bin/mysql-export.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 _os="`uname`"
-_now=$(date +"%m_%d_%Y")
-_file="wp-data/data_$_now.sql"
+_now=$(date +"%Y%m%d")
+_file="./data/data_$_now.sql"
 
 # Export dump
 EXPORT_COMMAND='exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"'
